24a4c10a5de6e4c4209d9dc0abd2586fdf2a5390
antoinechedin <antoine.chedin@gmail.com>
2019-11-20 19:20:56 +0100

    Add moving platform

---
+++ Assets/Scripts/MovingSolid.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Solid))]
+public class MovingSolid : MonoBehaviour
+{
+    private Solid solid;
+
+    public float duration;
+    private Vector3 start;
+    private Vector3 end;
+    private float timer;
+
+    private void Awake()
+    {
+        solid = GetComponent<Solid>();
+        start = transform.GetChild(0).position;
+        end = transform.GetChild(1).position;
+    }
+
+
+    private void Update()
+    {
+        if (timer < duration / 2f)
+        {
+            Vector3 targetPos = Vector3.Lerp(start, end, timer * 2f / duration);
+            solid.Move(targetPos - transform.position);
+        }
+        else
+        {
+            Vector3 targetPos = Vector3.Lerp(end, start, timer * 2f / duration - 1);
+            solid.Move(targetPos - transform.position);
+        }
+
+
+        timer += Time.deltaTime;
+        timer %= duration;
+    }
+
+
+
+
+}
--- Assets/Scripts/Solid.cs
+++ Assets/Scripts/Solid.cs
@@ -2,10 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class Solid : MonoBehaviour
 {
-    public void Move(Vector2 move)
+    private BoxCollider2D boxCollider;
+
+    private float xRemainer = 0f;
+    private float yRemainer = 0f;
+
+    private void Awake()
     {
-        transform.Translate(move);
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    public void Move(Vector3 move)
+    {
+        Move(move.x, move.y);
+    }
+
+    public void Move(float x, float y)
+    {
+        xRemainer += x;
+        yRemainer += y;
+
+        int moveX = Mathf.RoundToInt(xRemainer);
+        int moveY = Mathf.RoundToInt(yRemainer);
+
+        if (moveX != 0 || moveY != 0)
+        {
+            Actor[] allActors = GameObject.FindObjectsOfType<Actor>();
+            List<Actor> ridingActor = new List<Actor>();
+            boxCollider.enabled = false;
+
+            if (moveX != 0)
+            {
+                xRemainer -= moveX;
+                transform.Translate(moveX, 0, 0);
+
+                foreach (Actor actor in allActors)
+                {
+                    if (boxCollider.IsTouching(actor.boxCollider))
+                    {
+                        actor.MoveX(
+                            boxCollider.size.x / 2f - actor.boxCollider.size.x / 2f,
+                            null
+                        );
+                    }
+                    else if (ridingActor.Contains(actor))
+                    {
+                        actor.MoveX(moveX, null);
+                    }
+                }
+            }
+
+            if (moveY != 0)
+            {
+                yRemainer -= moveY;
+                transform.Translate(moveY, 0, 0);
+
+                foreach (Actor actor in allActors)
+                {
+                    if (boxCollider.IsTouching(actor.boxCollider))
+                    {
+                        actor.MoveY(
+                            boxCollider.size.y / 2f - actor.boxCollider.size.y / 2f,
+                            null
+                        );
+                    }
+                    else if (ridingActor.Contains(actor))
+                    {
+                        actor.MoveY(moveY, null);
+                    }
+                }
+            }
+
+            boxCollider.enabled = true;
+        }
     }
 }
