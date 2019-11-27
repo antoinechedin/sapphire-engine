c3c6df1c6e004f7861296ae46455b12d5dd4cd2a
antoinechedin <antoine.chedin@gmail.com>
2019-11-27 11:30:45 +0100

    Add levers

---
+++ Assets/Scripts/Activable.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Activable : MonoBehaviour
+{
+    private Animator animator;
+
+    public bool isUsable;
+    public bool powered;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+
+        isUsable = false;
+        powered = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Action") && isUsable)
+        {
+            powered = !powered;
+            if (animator != null)
+            {
+                if (powered)
+                    animator.SetTrigger("enable");
+                else
+                    animator.SetTrigger("disable");
+            }
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+            isUsable = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+            isUsable = false;
+    }
+
+}
